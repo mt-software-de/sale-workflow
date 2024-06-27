d0d460bca08d84382d10124b0f9680ef55c0f717
Mathieu <mathieu.delva@hotmail.com>
2024-06-27 09:26:25 +0200

    [IMP] code review

--- sale_stock_prebook/models/stock_picking.py
+++ sale_stock_prebook/models/stock_picking.py
@@ -12,24 +12,6 @@ class StockPicking(models.Model):
         related="sale_id.stock_is_reserved"
     )
 
-
-    def _prepare_jump_to_sale_order(self, company_id):
-        action = self.env["ir.actions.actions"]._for_xml_id(
-            "sale.action_orders"
-        )
-        action = {
-                "name": "Sale Order",
-                "type": "ir.actions.act_window",
-                "res_model": "sale.order",
-                "view_type": "form",
-                "view_mode": "form",
-                "res_id": self.sale_id.id,
-                "target": "current",
-            }
-        return action
-
     def jump_to_sale_order(self):
         self.ensure_one()
-        company_id = self.company_id.id or self.env.company.id
-        action = self._prepare_jump_to_sale_order(company_id)
-        return action
+        return self.sale_id.get_formview_action()
